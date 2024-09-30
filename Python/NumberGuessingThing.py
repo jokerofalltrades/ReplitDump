f42f1b916a1c38cb36a18cfca566a7fd4f6f6d20
Daniel <21williamson@parmiters.herts.sch.uk>
2024-09-30 10:25:59 +0100

    Create NumberGuessingThing.py

---
+++ Python/NumberGuessingThing.py
@@ -0,0 +1,11 @@
+import random
+
+secretNum = random.randint(1,100)
+uInput = int(input("Guess a number between 1 and 100:"))
+
+while uInput != secretNum:
+    if uInput > secretNum:
+        uInput = int(input("Your guess was larger than the secret number, guess another number:"))
+    else:
+        uInput = int(input("Your guess was smaller than the secret number, guess another number:"))
+print("Wow! you guessed it.")
