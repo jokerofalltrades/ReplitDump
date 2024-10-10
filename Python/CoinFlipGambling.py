347a7eb4beb7a9a60db0ac15d91419fa865dc071
Daniel <21williamson@parmiters.herts.sch.uk>
2024-10-10 17:51:34 +0000

    Added Old Man to Coin Flip Game

--- Python/CoinFlipGambling.py
+++ Python/CoinFlipGambling.py
@@ -3,6 +3,7 @@ import random
 money = 100
 answerGiven = 0
 flips = 0
+oldManEncounters = 0
 
 print("The Flipper: Welcome to the world's best casino: We have one enthralling game here.")
 print("The Flipper: Our game is gambling on a coin flip. You start with 100 money.")
@@ -15,7 +16,8 @@ if hereBefore == "1":
         saveCode = bytes_obj.decode('utf-8')
         if saveCode.find("ValidSave:") != -1:
             money = int(saveCode[(saveCode.find("Money:")+6):(saveCode.find("Flips:"))])
-            flips = int(saveCode[(saveCode.find("Flips:")+6):len(saveCode)])
+            flips = int(saveCode[(saveCode.find("Flips:")+6):saveCode.find("OldMan:")])
+            oldManEncounters = int(saveCode[(saveCode.find("OldMan:")+7):len(saveCode)])
             answerGiven = 1
         elif saveCode == "":
             print("Mysterious Man: Continue on with my companion.")
@@ -28,7 +30,7 @@ while money > 0:
     bet = ""
     print(f"The Flipper: You currently have {money} money.")
     print("The Flipper: Do you want to bet on heads, tails, or landing on its edge.")
-    bet = input("The Flipper: Odds: Heads - x1.9 your stake, Tails - x1.9 your stake, Edge x500 your stake (Enter Heads, Tails or Edge)")
+    bet = input("The Flipper: Odds: Heads - x1.9 your stake, Tails - x1.9 your stake, Edge - x500 your stake (Enter Heads, Tails or Edge)")
     while stake >= money or stake == 0:
         stake = int(input("How much do you want to bet?"))
     bet = bet.lower()
@@ -53,13 +55,19 @@ while money > 0:
     if response == "3":
         response = input("The Flipper: Are you sure you want to leave? Press 1 to continue, 2 to get your save code and leave and 3 to just leave.")
         if response == "2":
-            newSaveCode = f"ValidSave:Money:{money}Flips:{flips}".encode("utf-8").hex()
+            newSaveCode = f"ValidSave:Money:{money}Flips:{flips}OldMan:{oldManEncounters}".encode("utf-8").hex()
             print(f"Mysterious Man: {newSaveCode}")
             break
         elif response == "3":
             break
     if response == "2":
-        newSaveCode = f"ValidSave:Money:{money}Flips:{flips}".encode("utf-8").hex()
+        newSaveCode = f"ValidSave:Money:{money}Flips:{flips}OldMan:{oldManEncounters}".encode("utf-8").hex()
         print(f"Mysterious Man: {newSaveCode}")
+    if random.randint(1,100) == 100:
+        if oldManEncounters == 0:
+            print("???: Psst... Hey Kid!")
+            print("Wise Old Man: Kid, don't bet on edge, the odds are 1 in 5000!")
+            print("Wise Old Man: These yung'uns could really use some help...")
+            oldManEncounters += 1
 
 print("The Flipper: Thanks for playing! See you again soon!")
