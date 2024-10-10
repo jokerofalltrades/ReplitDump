b81bf937e2a01a677070875dc34f0f46823ca074
Daniel <21williamson@parmiters.herts.sch.uk>
2024-10-10 08:55:43 +0000

    Create CoinFlipGambling.py

---
+++ Python/CoinFlipGambling.py
@@ -0,0 +1,12 @@
+import random
+
+money = 100
+
+print("The Flipper: Welcome to the world's worst casino: We have one game here.")
+print("The Flipper: Our game is gambling on a coin flip. You start with 100 money.")
+hereBefore = input("The Flipper: If you have wasted your time here before, enter 1, else enter 2.")
+if hereBefore == "1":
+    saveCode = input("Mysterious Man: Please enter your save code:")
+
+while money > 0:
+    pass
