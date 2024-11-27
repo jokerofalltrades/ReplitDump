bc99bbf6943cc7f149598ec5a2167e49efb8c4b4
Daniel <21williamson@parmiters.herts.sch.uk>
2024-11-27 22:31:02 +0000

    Update fizzbuzz code

--- Python/SimpleFizzBuzz.py
+++ Python/SimpleFizzBuzz.py
@@ -1,12 +1,12 @@
 number = 1
 uInput = int(input("What number would you like to go up to?"))
-while number <= uInput:
-  if number%15 == 0:
-    print("FizzBuzz")
-  elif number%3 == 0:
-    print("Fizz")
-  elif number%5 == 0:
-    print("Buzz")
-  else:
-    print(number)
-  number += 1
+for number in range(uInput):
+    output = ""
+    if number % 3 == 0:
+        output += "Fizz"
+    if number % 5 == 0:
+        output += "Buzz"
+    if output == "":
+        output = str(number)
+    print(output)
+    number += 1
