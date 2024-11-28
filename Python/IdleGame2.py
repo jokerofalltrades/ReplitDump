c82ef835598673d6402446b691e078b225880304
Daniel <21williamson@parmiters.herts.sch.uk>
2024-11-28 14:26:48 +0000

    fixed indents

--- Python/IdleGame2.py
+++ Python/IdleGame2.py
@@ -6,16 +6,15 @@ hydrogenProductionMulti = 1
 helium = 0
 
 def calculate(hydrogen, hydrogenPerSecond, hydrogenProductionMulti):
-  hydrogenProductionRate = round(hydrogenPerSecond / 10 * hydrogenProductionMulti, 2)
-  hydrogen = round(hydrogen + hydrogenProductionRate,2)
-  return hydrogen, hydrogenProductionRate
+	hydrogenProductionRate = round(hydrogenPerSecond / 10 * hydrogenProductionMulti, 2)
+	hydrogen = round(hydrogen + hydrogenProductionRate,2)
+	return hydrogen, hydrogenProductionRate
 
 def printEverything(hydrogen, hydrogenProductionRate):
-  print("Hydrogen: "+str(hydrogen)+" ("+str(hydrogenProductionRate*10)+ "/s)",end="\r")
-  #print("Helium: " + str(hydrogen) + " (" + str(hydrogenProductionRate) + "/s)")
+	print("Hydrogen: "+str(hydrogen)+" ("+str(hydrogenProductionRate*10)+ "/s)",end="\r")
+  	#print("Helium: " + str(hydrogen) + " (" + str(hydrogenProductionRate) + "/s)")
 
 while True:
-  hydrogen, hydrogenProductionRate = calculate(hydrogen, hydrogenPerSecond,
-hydrogenProductionMulti)
-  printEverything(hydrogen, hydrogenProductionRate)
-  time.sleep(0.1) 
+	hydrogen, hydrogenProductionRate = calculate(hydrogen, hydrogenPerSecond,hydrogenProductionMulti)
+	printEverything(hydrogen, hydrogenProductionRate)
+	time.sleep(0.1) 
