9a0aee352eb854abf5e24f37f6a66eab0a8d4103
Daniel <21williamson@parmiters.herts.sch.uk>
2025-02-27 09:43:22 +0000

    merge sort works!

--- Python/MergeSort.py
+++ Python/MergeSort.py
@@ -3,19 +3,29 @@ listOfNums = []
 while len(listOfNums) < 8:
     num = random.randint(1,100)
     if num not in listOfNums: listOfNums.append(num)
-global highscope
-highscope = 0
-def splitList(list, scope):
-    global highscope
-    scope += 1
-    newList = list
-    if highscope < scope: highscope = scope
-    if len(list)//2 >= 1:
-        newList = [list[:len(list)//2],list[len(list)//2:]]
-        newList = [splitList(newList[0],scope),splitList(newList[1],scope)]
-    return newList
-def mergeSort(list, scope):
-    scope -= 1
 
-print(splitList(listOfNums, 0))
-print(highscope)
+def mergeSort(list1, list2, n):
+    width = 1
+    while width < n:
+        i = 0
+        while i < n:
+            list1, list2 = merge(list1, i, min(i+width, n), min(i+width*2, n), list2)
+            i += 2*width
+        width *= 2
+        for i in range(n):
+            list1[i] = list2[i]
+    return list1
+
+def merge(list1, iLeft, iRight, iEnd, list2):
+    k,i,v = iLeft,iLeft,iRight
+    while k < iEnd:
+        if i < iRight and (v >= iEnd or list1[i] <= list1[v]):
+            list2[k] = list1[i]
+            i += 1
+        else:
+            list2[k] = list1[v]
+            v += 1
+        k += 1
+    return list1, list2
+
+print(mergeSort(listOfNums, [""]*len(listOfNums), len(listOfNums)))
